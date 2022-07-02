9772947f9c8c800c45672d49d28d3b2bcbe5e8a7
David.B <108198561+dave-b-isme@users.noreply.github.com>
2022-07-02 13:07:45 -0700

    New Branch - Challenge 01

---
+++ RevitAddinAcademy_01/Command01Challenge.cs
@@ -0,0 +1,34 @@
+#region Namespaces
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+#endregion
+
+namespace RevitAddinAcademy_01
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command01Challenge : IExternalCommand
+    {
+        public Result Execute(
+          ExternalCommandData commandData,
+          ref string message,
+          ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+
+            //Comment Line
+            TaskDialog.Show("Revit Add-in Academy", "This is just a command add-in dialog box");
+
+            return Result.Succeeded;
+        }
+    }
+}
